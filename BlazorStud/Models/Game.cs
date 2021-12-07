f8d4b35b4e1634b3ec2a9a76771764306ae13dc4
nacharvs <r3dinc@yandex.ru>
2021-12-07 18:20:37 +0300

    start

---
+++ BlazorStud/Models/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorStud.Models
+{
+    public class Game
+    {
+       
+        public Game(string name, string genre, DateTime releaseDate)
+        {
+            Name = name;
+            Genre = genre;
+            ReleaseDate = releaseDate;
+        }
+
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public static List<Game> SimpleList()
+        {
+            return new List<Game>()
+            {
+                new Game("farm","strategy", new DateTime(2019, 12, 15)),
+                 new Game("piupiy","strategy", new DateTime(2019, 12, 15)),
+                  new Game("crudge","strategy", new DateTime(2019, 12, 15))
+            };
+        }
+    }
+}
